705a323ca67f5bb223af717884522ccc4afeb6e1
Francisco Kahil <codexico@gmail.com>
2016-05-06 02:40:50 -0300

    Add install script and instructions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dotfiles
 Basic dotfiles starter
+
+```
+sudo apt-get install git
+
+git clone git@github.com:codexico/dotfiles.git
+
+cd dotfiles
+
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "creating symlinks";
+
+ln -s .bash_aliases ~/.bash_aliases
+ln -s .csscomb.json ~/.csscomb.json
+ln -s .editorconfig ~/.editorconfig
+ln -s .eslintrc ~/.eslintrc
+ln -s .gitconfig ~/.gitconfig
+ln -s .gitignore_global ~/.gitignore_global
+ln -s .jshintrc ~/.jshintrc
