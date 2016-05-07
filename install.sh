4dfe827d8c82381f57fa3844b741d5a1abe0b9f7
Francisco Kahil <codexico@gmail.com>
2016-05-07 15:00:32 -0300

    Add node install

--- install.sh
+++ install.sh
@@ -73,3 +73,13 @@ sudo apt-get install -y chromium-browser gpodder skype vlc yakuake youtube-dl
 echo "installing development software";
 sudo apt-get install -y \
     git-core git-gui gitk vim curl
+
+echo "installing node with nvm";
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+# install last node
+nvm install node
+# set last node as default
+nvm alias default node
+# install some useful global packages
+npm install -g http-server livereload
