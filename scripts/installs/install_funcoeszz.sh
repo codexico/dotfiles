5fe96173454888e4ee7a46fc4e568290c6f2203d
Francisco Kahil <codexico@gmail.com>
2021-08-01 22:05:19 -0300

    Add PATH instructions

--- scripts/installs/install_funcoeszz.sh
+++ scripts/installs/install_funcoeszz.sh
@@ -14,31 +14,33 @@
 #     - verificar se já está instalada chamar o zzzz --atualiza
 # ----------------------------------------------------------------------------
 
-echo "instalando dependências das funcoeszz";
-sudo apt install lynx;
+echo "instalando dependências das funcoeszz"
+sudo apt install lynx
 
-echo "instalando as funcoeszz";
+echo "instalando as funcoeszz"
 
-LAST_ZZ_VERSION=$(curl http://funcoeszz.net/v);
-LAST_ZZ_URL="http://funcoeszz.net/download/funcoeszz-$LAST_ZZ_VERSION.sh";
+LAST_ZZ_VERSION=$(curl http://funcoeszz.net/v)
+LAST_ZZ_URL="http://funcoeszz.net/download/funcoeszz-$LAST_ZZ_VERSION.sh"
 curl -L -o funcoeszz $LAST_ZZ_URL
 
 # seria melhor testar a existência dos arquivos ~/.bashrc e ~/.zshrc?
 if [ "$SHELL" == "/bin/bash" ]; then
-  echo "instalando no bash";
+  echo "instalando no bash"
   chmod +x funcoeszz
   mv funcoeszz ~/bin/funcoeszz
   cd ~/bin
   ./funcoeszz zzzz --bashrc
-  echo "execute 'source ~/.bashrc' ou abra outro terminal";
+  echo "execute 'source ~/.bashrc' ou abra outro terminal"
 elif [ "$SHELL" == "/usr/bin/zsh" ]; then
-  echo "instalando no zsh";
+  echo "instalando no zsh"
   chmod +x funcoeszz
   mv funcoeszz ~/bin/funcoeszz
   cd ~/bin
   ./funcoeszz zzzz --zshrc
-  echo "execute 'source ~/.zshrc' ou abra outro terminal";
+  echo "execute 'source ~/.zshrc' ou abra outro terminal"
 else
-  echo "Não sei qual o seu shell!";
-  echo "Por favor acesse o FAQ: http://funcoeszz.net/faq.html";
+  echo "Não sei qual o seu shell!"
+  echo "Por favor acesse o FAQ: http://funcoeszz.net/faq.html"
 fi
+
+echo "não esqueça de adicionar ~/bin ao seu PATH"
